970ea1c50c215d5e086afd0591c51ae2d2eb2ef3
sudo-nan0-RaySK <saksham.sethi2017@vitstudent.ac.in>
2020-11-04 21:26:51 +0530

    feat: RCE finished

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -9,6 +10,7 @@ var rceRouter = require('./routes/rce');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
