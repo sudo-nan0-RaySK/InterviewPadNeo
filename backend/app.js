99abcff4848a8b15060e5181aba94cd8b62bde0f
sudo-nan0-RaySK <saksham.sethi2017@vitstudent.ac.in>
2020-11-06 02:05:03 +0530

    feat: Collaborative editing completed

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var http = require('http');
+var socket = require('socket.io')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var rceRouter = require('./routes/rce');
 
+/** Express Stuff */
+
 var app = express();
+
+app.set('port', 5000);
+
+const server = http.createServer(app);
 
 app.use(cors());
 app.use(logger('dev'));
@@ -19,6 +27,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/rce',rceRouter);
+app.use('/rce', rceRouter);
+
+
+/** Socket Stuff */
+
+const io = socket.listen(server);
+
+io.on('connection', (client) => {
+    console.log('A client has connected', client.id);
+    client.on('codeChange', ({newCode}) => {
+        console.log('New code recived from', client.id, 'code:',newCode);
+        io.sockets.emit('codeChange', { newCode });
+    })
+});
+
+server.listen(5000, () => {
+    console.log('Server is now running on port 5000')
+});
 
 module.exports = app;
